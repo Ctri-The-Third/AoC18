ef1c3f30272a07828fa391163eed6e63256618fa
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2018-12-06 15:29:02 +0000

    Projected created

---
+++ AoC18 core v3/AoC18 core v3/Controllers/DaysController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AoC18_core_v3.Controllers
+{
+    public class DaysController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Day1()
+        {
+            AoC18_core_v3.
+            ViewData["Problem"] = "sample";
+            ViewData["Solution"] = "sample2";
+            return View();
+        }
+    }
+}
---
+++ AoC18 core v3/AoC18 core v3/wwwroot/Day1Code/Day1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AoC18.wwwroot.Day1Code
+{
+    public class Day1
+{
+     /*For example, if the device displays frequency changes of +1, -2, +3, +1, then starting from a frequency of zero, the following changes would occur:
+
+    Current frequency  0, change of +1; resulting frequency  1.
+    Current frequency  1, change of -2; resulting frequency -1.
+    Current frequency -1, change of +3; resulting frequency  2.
+    Current frequency  2, change of +1; resulting frequency  3.
+    In this example, the resulting frequency is 3.*/
+    public static int parse (String text)
+    {
+        var frequency = 0;
+        //asume each line is broken up by a \n
+        var instructions = text.Split("\n");
+        foreach (var instruction in instructions)
+        {
+            //is it positive or negative?
+            var direction = direction.Substring(1);
+            var frequencyMod = int.Parse(direction.substring(direction.length - 1));
+            if (direction == "+")
+            {
+                frequency += frequencyMod;
+            }
+            else if (direciont == "-")
+            {
+                frequency -= frequencyMod;
+            }
+            return frequency;
+        }
+
+    }
+
+}
+}
